d3468b7cc52463b81c8c080d09c49dfd7ee158d7
Amit Kumar <amitkumar@Amits-MacBook-Air-2.local>
2026-04-20 17:02:51 +0530

    first commit

---
+++ src/main/java/com/experiment/devops_specials/DevopsSpecialsApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.devops_specials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsSpecialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsSpecialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experiment/devops_specials/DevopsSpecialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.devops_specials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsSpecialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
